e9aac8204764ae8e7f16a68158f0451fc6f69c1c
github username <github email address>
2018-03-27 11:59:47 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,74 @@
+require "pry"
+
 class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    
+    new_student = self.new  # self.new is the same as running Song.new
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+     SELECT * FROM students
+    SQL
+    all_students = DB[:conn].execute(sql)
+    all_students.map do |student|
+      self.new_from_db(student)
+    end 
   end
+
+  def self.first_X_students_in_grade_10(students)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 LIMIT 2
+    SQL
+    DB[:conn].execute(sql)
+  end 
+
+   def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10
+    SQL
+    student = DB[:conn].execute(sql)
+    new_student = self.new_from_db(student[0])
+  end 
+
+  def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql, grade)
+  end
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 9
+    SQL
+    students = DB[:conn].execute(sql)
+    students
+  end 
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql)
+  end 
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ?
+    SQL
+    found_student = DB[:conn].execute(sql, name)
+    self.new_from_db(found_student[0])
   end
   
   def save
